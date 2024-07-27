f71867bc2466b13304b974b320d391c4169de2f4
Petunia675 <ogekepurity675@gmail.com>
2024-07-27 20:17:48 +0300

    create reducer fix bug

--- frontend/src/redux/reducers/cart.js
+++ frontend/src/redux/reducers/cart.js
@@ -1,4 +1,8 @@
-import { createReducer } from "@reduxjs/toolkit";
+import { createReducer, createAction } from "@reduxjs/toolkit";
+
+// Define actions
+export const addToCart = createAction('cart/addToCart');
+export const removeFromCart = createAction('cart/removeFromCart');
 
 const initialState = {
   cart: localStorage.getItem("cartItems")
@@ -6,27 +10,22 @@ const initialState = {
     : [],
 };
 
-export const cartReducer = createReducer(initialState, {
-  addToCart: (state, action) => {
-    const item = action.payload;
-    const isItemExist = state.cart.find((i) => i._id === item._id);
-    if (isItemExist) {
-      return {
-        ...state,
-        cart: state.cart.map((i) => (i._id === isItemExist._id ? item : i)),
-      };
-    } else {
-      return {
-        ...state,
-        cart: [...state.cart, item],
-      };
-    }
-  },
-
-  removeFromCart: (state, action) => {
-    return {
-      ...state,
-      cart: state.cart.filter((i) => i._id !== action.payload),
-    };
-  },
+export const cartReducer = createReducer(initialState, (builder) => {
+  builder
+    .addCase(addToCart, (state, action) => {
+      const item = action.payload;
+      const isItemExist = state.cart.find((i) => i._id === item._id);
+      if (isItemExist) {
+        state.cart = state.cart.map((i) => (i._id === isItemExist._id ? item : i));
+      } else {
+        state.cart.push(item);
+      }
+      // Optionally, update localStorage
+      localStorage.setItem("cartItems", JSON.stringify(state.cart));
+    })
+    .addCase(removeFromCart, (state, action) => {
+      state.cart = state.cart.filter((i) => i._id !== action.payload);
+      // Optionally, update localStorage
+      localStorage.setItem("cartItems", JSON.stringify(state.cart));
+    });
 });
--- frontend/src/redux/reducers/event.js
+++ frontend/src/redux/reducers/event.js
@@ -1,64 +1,83 @@
-import { createReducer } from "@reduxjs/toolkit";
+import { createReducer, createAction } from "@reduxjs/toolkit";
+
+// Define actions
+export const eventCreateRequest = createAction('event/eventCreateRequest');
+export const eventCreateSuccess = createAction('event/eventCreateSuccess');
+export const eventCreateFail = createAction('event/eventCreateFail');
+
+export const getAlleventsShopRequest = createAction('event/getAlleventsShopRequest');
+export const getAlleventsShopSuccess = createAction('event/getAlleventsShopSuccess');
+export const getAlleventsShopFailed = createAction('event/getAlleventsShopFailed');
+
+export const deleteeventRequest = createAction('event/deleteeventRequest');
+export const deleteeventSuccess = createAction('event/deleteeventSuccess');
+export const deleteeventFailed = createAction('event/deleteeventFailed');
+
+export const getAlleventsRequest = createAction('event/getAlleventsRequest');
+export const getAlleventsSuccess = createAction('event/getAlleventsSuccess');
+export const getAlleventsFailed = createAction('event/getAlleventsFailed');
+
+export const clearErrors = createAction('event/clearErrors');
 
 const initialState = {
   isLoading: true,
+  events: [],
+  event: null,
+  success: false,
+  error: null,
+  message: '',
+  allEvents: []
 };
 
-export const eventReducer = createReducer(initialState, {
-  eventCreateRequest: (state) => {
-    state.isLoading = true;
-  },
-  eventCreateSuccess: (state, action) => {
-    state.isLoading = false;
-    state.event = action.payload;
-    state.success = true;
-  },
-  eventCreateFail: (state, action) => {
-    state.isLoading = false;
-    state.error = action.payload;
-    state.success = false;
-  },
-
-  // get all events of shop
-  getAlleventsShopRequest: (state) => {
-    state.isLoading = true;
-  },
-  getAlleventsShopSuccess: (state, action) => {
-    state.isLoading = false;
-    state.events = action.payload;
-  },
-  getAlleventsShopFailed: (state, action) => {
-    state.isLoading = false;
-    state.error = action.payload;
-  },
-
-  // delete event of a shop
-  deleteeventRequest: (state) => {
-    state.isLoading = true;
-  },
-  deleteeventSuccess: (state, action) => {
-    state.isLoading = false;
-    state.message = action.payload;
-  },
-  deleteeventFailed: (state, action) => {
-    state.isLoading = false;
-    state.error = action.payload;
-  },
-
-  // get all events 
-  getAlleventsRequest: (state) => {
-    state.isLoading = true;
-  },
-  getAlleventsSuccess: (state, action) => {
-    state.isLoading = false;
-    state.allEvents = action.payload;
-  },
-  getAlleventsFailed: (state, action) => {
-    state.isLoading = false;
-    state.error = action.payload;
-  },
-
-  clearErrors: (state) => {
-    state.error = null;
-  },
+export const eventReducer = createReducer(initialState, (builder) => {
+  builder
+    .addCase(eventCreateRequest, (state) => {
+      state.isLoading = true;
+    })
+    .addCase(eventCreateSuccess, (state, action) => {
+      state.isLoading = false;
+      state.event = action.payload;
+      state.success = true;
+    })
+    .addCase(eventCreateFail, (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload;
+      state.success = false;
+    })
+    .addCase(getAlleventsShopRequest, (state) => {
+      state.isLoading = true;
+    })
+    .addCase(getAlleventsShopSuccess, (state, action) => {
+      state.isLoading = false;
+      state.events = action.payload;
+    })
+    .addCase(getAlleventsShopFailed, (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload;
+    })
+    .addCase(deleteeventRequest, (state) => {
+      state.isLoading = true;
+    })
+    .addCase(deleteeventSuccess, (state, action) => {
+      state.isLoading = false;
+      state.message = action.payload;
+    })
+    .addCase(deleteeventFailed, (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload;
+    })
+    .addCase(getAlleventsRequest, (state) => {
+      state.isLoading = true;
+    })
+    .addCase(getAlleventsSuccess, (state, action) => {
+      state.isLoading = false;
+      state.allEvents = action.payload;
+    })
+    .addCase(getAlleventsFailed, (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload;
+    })
+    .addCase(clearErrors, (state) => {
+      state.error = null;
+    });
 });
