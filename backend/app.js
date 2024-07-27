68f9343a90d7d592c20fa402b44f360463ee7d9f
Petunia675 <ogekepurity675@gmail.com>
2024-07-27 17:43:00 +0300

    added url

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 app.use(cors({
-  origin: ['http://localhost:3000',],
+  origin: ['https://e-shop-3yun.vercel.app/',],
   credentials: true
 }));
 
